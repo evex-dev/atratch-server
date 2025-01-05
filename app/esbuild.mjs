e1cced62df036fe8fe4dab0ca3bc7ad6d3293f9d
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-01-05 19:12:45 +0900

    ビルドスクリプトを修正

--- app/esbuild.mjs
+++ app/esbuild.mjs
@@ -1,13 +1,18 @@
 import esbuild from "esbuild";
-
+import { builtinModules } from "node:module";
 esbuild
 	.build({
 		entryPoints: ["./src/index.ts"],
 		outdir: "./dist",
 		bundle: true,
 		platform: "node",
-		format: "esm",
+		format: "cjs",
 		target: "esnext",
-		resolveExtensions: [".ts", ".js"],
+		external: [
+			...builtinModules, // Node.js 標準モジュール
+			...builtinModules.map((mod) => `node:${mod}`), // ESM対応
+			//"*", // すべてのモジュールを外部化
+		  ],
+		resolveExtensions: [".ts", ".js"],logLevel:"info"
 	})
 	.catch(() => process.exit(1));
